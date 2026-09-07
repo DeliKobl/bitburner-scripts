3c201e25a5353514fc2992fd07dadc7ad35cb58b
DeliKobl <deli@kobl.ca>
2026-09-07 02:46:26 -0700

    I worked on some stuff in lib hehe

--- lib/lib.ts
+++ lib/lib.ts
@@ -1,5 +1,12 @@
 import { NS, Player, Server } from "@ns";
 
+const HACK = "/lib/worker-hack.ts";
+const WEAKEN = "/lib/worker-weaken.ts";
+const GROW = "/lib/worker-grow.ts";
+const HACK_RAM = 1.7;
+const WEAKEN_RAM = 1.75;
+const GROW_RAM = 1.75;
+
 /** @public */
 export function getHostnames(ns: NS): string[] {
     let scan = ns.scan("home");
@@ -89,24 +96,91 @@ export function getRoot(ns: NS, servers: Server[]): void {
 }
 
 /** @public */
-export function findBestServerFormula(ns: NS, servers: Server[], player: Player): Server {
-    let bestServer = ns.formulas.mockServer();
-    let bestProfit = 0;
-    let profit = 0;
-    for (const server of servers) {
-        profit = calculateProfit(ns, server, player);
-        ns.tprintf("%s - $%f/s", server.hostname, profit);
-        if (profit < bestProfit) continue;
+function getHackSetup(ns: NS, servers: Server[], player: Player, runner: Server): HackSetup {
 
-        bestServer = server;
-        bestProfit = profit;
+    const weakenSecurityAmount = ns.formulas.hacking.weakenEffect(1, 1);
+    let mock = ns.formulas.mockServer()
+    let setup: HackSetup = {
+        target: ns.getServer("n00dles"),
+        runner: ns.getServer("home"),
+        threads: {
+            hackThreads: 1,
+            firstWeakenThreads: 1,
+            growThreads: 1,
+            secondWeakenThreads: 1
+        }
+    };
+    let profits = new Map<Server, number>();
+    let setups = new Map<Server, HWGW>();
+
+    for (const server of servers) {
+        if (server.requiredHackingSkill === undefined || server.requiredHackingSkill > player.skills.hacking || server.moneyMax === undefined) continue;
+        let idealThreads: HWGW = {
+            hackThreads: 1,
+            firstWeakenThreads: 1,
+            growThreads: 1,
+            secondWeakenThreads: 1
+        };
+        // time in seconds it takes for a batch to complete
+        const time = ns.formulas.hacking.weakenTime(server, player) / 1000;
+
+        mock.hackDifficulty = server.minDifficulty;
+        mock.requiredHackingSkill = server.requiredHackingSkill;
+        mock.moneyMax = server.moneyMax;
+        mock.serverGrowth = server.serverGrowth;
+        const hackChance = ns.formulas.hacking.hackChance(mock, player);
+
+        let bestProfit = 0
+
+        for (let threads = 1; threads < Infinity; threads++) {
+            const hackSecurityAmount = ns.hackAnalyzeSecurity(threads);
+            const amount = Math.min(ns.formulas.hacking.hackPercent(server, player) * threads * server.moneyMax, server.moneyMax);
+            mock.moneyAvailable = server.moneyMax - amount;
+            const growThreads = ns.formulas.hacking.growThreads(mock, player, mock.moneyMax)
+            const firstWeakenThreads = Math.ceil(hackSecurityAmount / weakenSecurityAmount);
+            const growSecurityAmount = ns.growthAnalyzeSecurity(growThreads);
+            const secondWeakenThreads = Math.ceil(growSecurityAmount / weakenSecurityAmount);
+            const hwgwram = (threads * HACK_RAM) + ((firstWeakenThreads + secondWeakenThreads) * WEAKEN_RAM) + (growThreads * GROW_RAM);
+            if (hwgwram > (runner.maxRam - runner.ramUsed)) break;
+
+            const profit = amount / time * hackChance * 60;
+            if (profit < bestProfit) continue;
+
+            bestProfit = profit;
+            idealThreads = {
+                hackThreads: threads,
+                firstWeakenThreads: firstWeakenThreads,
+                growThreads: growThreads,
+                secondWeakenThreads: secondWeakenThreads
+            };
+        }
+        profits.set(server, bestProfit);
+        setups.set(server, idealThreads)
     }
 
-    return bestServer;
+    const profitsSorted = new Map([...profits.entries()].sort((a, b) => a[1] - b[1]));
+    for (const x of profitsSorted.entries()) {
+        ns.tprintf("%s\n= $%s/min\n\n", x[0].hostname, ns.format.number(x[1]))
+        ns.tprintf("%s", String(setups.get(x[0])))
+    }
+
+
+    return setup;
+
 }
 
-function calculateProfit(ns: NS, server: Server, player: Player): number {
-    const time = ns.formulas.hacking.weakenTime(server, player)
-    const amount = ns.formulas.hacking.hackPercent(server, player) * ns.getServerMaxMoney(server.hostname);
-    return amount / time * 1000;
+
+/** @public */
+export interface HackSetup {
+    target: Server,
+    runner: Server,
+    threads: HWGW
+}
+
+/** @public */
+export interface HWGW {
+    hackThreads: number,
+    firstWeakenThreads: number,
+    growThreads: number,
+    secondWeakenThreads: number
 }
