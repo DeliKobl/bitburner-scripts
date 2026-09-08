6394ac9de2acf655d39b2cb4bdd72ba801105e7d
DeliKobl <deli@kobl.ca>
2026-09-08 02:14:52 -0700

    getHackSetup() is complete

--- lib/lib.ts
+++ lib/lib.ts
@@ -96,54 +96,80 @@ export function getRoot(ns: NS, servers: Server[]): void {
 }
 
 /** @public */
-function getHackSetup(ns: NS, servers: Server[], player: Player, runner: Server): HackSetup {
+export function getHackSetup(ns: NS, servers: Server[], player: Player, runner: Server): HackSetup {
 
-    const weakenSecurityAmount = ns.formulas.hacking.weakenEffect(1, 1);
+    // amount of security decrease for 1 thread
+    const weakenSecurityAmount = ns.formulas.hacking.weakenEffect(1, runner.cpuCores);
+
+    // mock server that will simulate different server states
     let mock = ns.formulas.mockServer()
-    let setup: HackSetup = {
-        target: ns.getServer("n00dles"),
-        runner: ns.getServer("home"),
-        threads: {
-            hackThreads: 1,
-            firstWeakenThreads: 1,
-            growThreads: 1,
-            secondWeakenThreads: 1
-        }
-    };
+
+    // map of profits for printing to the terminal
     let profits = new Map<Server, number>();
+
+    // map of ideal batch thread counts for each server.
+    // this is probably not needed
     let setups = new Map<Server, HWGW>();
 
     for (const server of servers) {
-        if (server.requiredHackingSkill === undefined || server.requiredHackingSkill > player.skills.hacking || server.moneyMax === undefined) continue;
-        let idealThreads: HWGW = {
-            hackThreads: 1,
-            firstWeakenThreads: 1,
-            growThreads: 1,
-            secondWeakenThreads: 1
-        };
+        // don't do anything on inelligible servers
+        if (
+            server.requiredHackingSkill === undefined ||
+            server.requiredHackingSkill > player.skills.hacking ||
+            server.moneyMax === undefined ||
+            server.moneyMax < 1 ||
+            !server.hasAdminRights
+        ) continue;
+
+        // this will go on the setups map
+        let idealThreads: HWGW | null = null
+
         // time in seconds it takes for a batch to complete
         const time = ns.formulas.hacking.weakenTime(server, player) / 1000;
 
+        // set some values on the mock server for the thread finding loop
         mock.hackDifficulty = server.minDifficulty;
         mock.requiredHackingSkill = server.requiredHackingSkill;
         mock.moneyMax = server.moneyMax;
         mock.serverGrowth = server.serverGrowth;
+        mock.hasAdminRights = server.hasAdminRights;
         const hackChance = ns.formulas.hacking.hackChance(mock, player);
+        const hackPercent = ns.formulas.hacking.hackPercent(mock, player);
 
         let bestProfit = 0
 
-        for (let threads = 1; threads < Infinity; threads++) {
-            const hackSecurityAmount = ns.hackAnalyzeSecurity(threads);
-            const amount = Math.min(ns.formulas.hacking.hackPercent(server, player) * threads * server.moneyMax, server.moneyMax);
+        // this loop is very inefficient! basically we need to find a set of four integers (a, b, c, d) that solves the equation
+        // (a * HACK_RAM) + ((b + c) * WEAKEN_RAM) + (d * GROW_RAM) <= (runner.maxRam - runner.ramUsed)
+        // while maximizing the value a.
+        // the values b, c, and d all depend on a.
+        // this loop starts at a = 1 and increments until the equation is no longer true.
+        // there is absolutely better ways to do this but I can't think of any right now.
+
+        // TODO: there is possibly a way to do this by finding out *how* b, c, and d relate to a
+        // TODO: and find a function that can solve this in one go but that is a project for another time.
+        for (let threads = 1; hackPercent * threads < 1; threads++) {
+
+            // amount stolen from the server
+            const amount = hackPercent * threads * server.moneyMax;
+
+            // calculate the amount of threads needed to grow the theoretical result of a hack back to full cash
             mock.moneyAvailable = server.moneyMax - amount;
-            const growThreads = ns.formulas.hacking.growThreads(mock, player, mock.moneyMax)
+            const growThreads = ns.formulas.hacking.growThreads(mock, player, mock.moneyMax, runner.cpuCores)
+
+            // weaken threads, pretty simple
+            const hackSecurityAmount = ns.hackAnalyzeSecurity(threads);
             const firstWeakenThreads = Math.ceil(hackSecurityAmount / weakenSecurityAmount);
-            const growSecurityAmount = ns.growthAnalyzeSecurity(growThreads);
+            const growSecurityAmount = ns.growthAnalyzeSecurity(growThreads, undefined, runner.cpuCores);
             const secondWeakenThreads = Math.ceil(growSecurityAmount / weakenSecurityAmount);
+
+            // check if we're over budget on ram cost and stop if we are
             const hwgwram = (threads * HACK_RAM) + ((firstWeakenThreads + secondWeakenThreads) * WEAKEN_RAM) + (growThreads * GROW_RAM);
             if (hwgwram > (runner.maxRam - runner.ramUsed)) break;
 
-            const profit = amount / time * hackChance * 60;
+            const batches = Math.floor((runner.maxRam - runner.ramUsed) / hwgwram);
+
+            // this profit number is the $/min, formatted this way for human readability
+            const profit = amount * batches / time * hackChance * 60;
             if (profit < bestProfit) continue;
 
             bestProfit = profit;
@@ -151,21 +177,49 @@ function getHackSetup(ns: NS, servers: Server[], player: Player, runner: Server)
                 hackThreads: threads,
                 firstWeakenThreads: firstWeakenThreads,
                 growThreads: growThreads,
-                secondWeakenThreads: secondWeakenThreads
+                secondWeakenThreads: secondWeakenThreads,
             };
         }
+        if (!idealThreads) {
+            // this should never happen but is in place to make the compiler happy
+            ns.tprintf("Error discerning ideal thread count!");
+            ns.exit();
+        }
+
+        // set the best profit and ideal threads on the maps
+        // TODO: find a method to have both these values on one map, or some other data structure
         profits.set(server, bestProfit);
-        setups.set(server, idealThreads)
+        setups.set(server, idealThreads);
     }
 
     const profitsSorted = new Map([...profits.entries()].sort((a, b) => a[1] - b[1]));
-    for (const x of profitsSorted.entries()) {
-        ns.tprintf("%s\n= $%s/min\n\n", x[0].hostname, ns.format.number(x[1]))
-        ns.tprintf("%s", String(setups.get(x[0])))
+    const profitsSortedIter = profitsSorted.entries()
+    for (const x of profitsSortedIter) {
+        ns.tprintf("%s\n= $%s/min  (Level %f required)  {%i, %i, %i, %i}",
+            x[0].hostname,
+            ns.format.number(x[1]),
+            x[0].requiredHackingSkill,
+            setups.get(x[0])?.hackThreads,
+            setups.get(x[0])?.firstWeakenThreads,
+            setups.get(x[0])?.growThreads,
+            setups.get(x[0])?.secondWeakenThreads,
+        )
+        ns.tprintf("\n");
     }
 
+    // pop off the last element from the sorted map to get best profit server
+    const bestServer = Array.from(profitsSorted.keys()).pop();
+    if (!bestServer) ns.exit();
 
-    return setup;
+    // associated thread setup
+    const bestThreads = setups.get(bestServer)
+    if (!bestThreads) ns.exit();
+
+    return {
+        target: bestServer,
+        runner: runner,
+        threads: bestThreads,
+    };
 
 }
 
@@ -174,7 +228,7 @@ function getHackSetup(ns: NS, servers: Server[], player: Player, runner: Server)
 export interface HackSetup {
     target: Server,
     runner: Server,
-    threads: HWGW
+    threads: HWGW,
 }
 
 /** @public */
@@ -182,5 +236,5 @@ export interface HWGW {
     hackThreads: number,
     firstWeakenThreads: number,
     growThreads: number,
-    secondWeakenThreads: number
+    secondWeakenThreads: number,
 }
